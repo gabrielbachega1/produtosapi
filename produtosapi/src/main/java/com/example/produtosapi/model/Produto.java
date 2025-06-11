af3ce849704b58642b8566cf0363d3d82dd9c962
gabrielbachega1 <bachegagabriel@gmail.com>
2025-06-11 18:11:51 -0300

    primeiro commit

---
+++ produtosapi/src/main/java/com/example/produtosapi/model/Produto.java
@@ -0,0 +1,52 @@
+package com.example.produtosapi.model;
+
+import jakarta.persistence.Entity;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
---
+++ produtosapi/target/classes/data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE produto (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    descricao VARCHAR(500)
+);
